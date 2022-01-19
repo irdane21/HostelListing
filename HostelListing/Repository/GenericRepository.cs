def3fdd6efd04becf097e7ea87141585728f0597
Adrien LEFRANCQ <alefrancq@redoute.fr>
2022-01-19 17:49:24 +0100

    WIP service repository

---
+++ HostelListing/Repository/GenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using HostelListing.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task Delete(int id)
+        {
+
+        }
+    }
+}
